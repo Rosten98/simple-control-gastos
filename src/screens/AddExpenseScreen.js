232abab533b67f246465fca946279039cb603739
Gerardo Rodriguez <grdwebdev@gmail.com>
2023-10-07 12:11:58 -0600

    Add fonts and improve AddExpense form

--- src/screens/AddExpenseScreen.js
+++ src/screens/AddExpenseScreen.js
@@ -7,10 +7,21 @@ import {
   TextInput,
   Button,
 } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const AddExpenseScreen = (props) => {
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState('');
+  // Dropdown picker
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    // {label: 'Comida', value: 'comida', icon: 'food'},
+    // {label: 'Transporte', value: 'transporte', icon: 'taxi'}
+    {label: 'Comida', value: 'comida', icon: ()=> <MaterialCommunityIcons name='food' size={20}/>},
+    {label: 'Transporte', value: 'transporte', icon: ()=> <MaterialCommunityIcons name='taxi' size={20}/>}
+  ]);
 
   const titleChangedHandler = (text) => {
     setTitle(text);
@@ -32,17 +43,9 @@ const AddExpenseScreen = (props) => {
   };
 
   return (
-    <ScrollView style={styles.form}>
+    <View style={styles.form}>
       <View style={styles.formControl}>
-        <Text style={styles.label}>Title</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={titleChangedHandler}
-          value={title}
-        />
-      </View>
-      <View style={styles.formControl}>
-        <Text style={styles.label}>Amount</Text>
+        <Text style={styles.label}>Costo: </Text>
         <TextInput
           style={styles.input}
           onChangeText={amountChangedHandler}
@@ -50,16 +53,38 @@ const AddExpenseScreen = (props) => {
           keyboardType="numeric"
         />
       </View>
-      <View style={styles.button}>
-        <Button title="Add Expense" onPress={addExpenseHandler} />
+      <View style={styles.formControl}>
+        <Text style={styles.label}>Descripción: </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={titleChangedHandler}
+          value={title}
+        />
       </View>
-    </ScrollView>
+      <View style={styles.formControl}>
+        <Text style={styles.label}>Categoría: </Text>
+        <DropDownPicker
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          style={styles.input}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button title="Add Expense" onPress={addExpenseHandler}/>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   form: {
-    margin: 20,
+    padding: 20,
+    backgroundColor: 'white',
+    height: '100%'
   },
   formControl: {
     width: '100%',
@@ -67,12 +92,15 @@ const styles = StyleSheet.create({
   },
   label: {
     marginVertical: 8,
+    fontFamily: 'WorkSans-Regular',
   },
   input: {
-    paddingHorizontal: 2,
-    paddingVertical: 5,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
+    fontFamily: 'WorkSans-Regular',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderColor: '#CDCDCD',
+    borderWidth: 1,
+    borderRadius: 10,
   },
   button: {
     marginTop: 20,
