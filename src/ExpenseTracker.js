7c07dda63e4d6b35a985a984151bb8c7e5defb6d
Gerardo Rodriguez <grdwebdev@gmail.com>
2023-10-07 14:29:37 -0600

    Add HomeScreen and rearrange menu

--- src/ExpenseTracker.js
+++ src/ExpenseTracker.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import ListExpensesScreen from './screens/ListExpensesScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const ExpenseTracker = () => {
   const Tab = createBottomTabNavigator();
@@ -13,22 +14,32 @@ const ExpenseTracker = () => {
       <Tab.Navigator>
         <Tab.Screen 
           name="Inicio" 
-          component={AddExpenseScreen} 
+          component={HomeScreen} 
           options={{
             tabBarLabel: 'Inicio',
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="home" color={color} size={size} />
             ),
           }}/>
-          <Tab.Screen
-            name="Gastos"
-            component={ListExpensesScreen}
-            options={{
-              tabBarLabel: 'Gastos',
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="view-list" color={color} size={size} />
-              ),
-            }}/>
+        <Tab.Screen
+          name="Gastos"
+          component={ListExpensesScreen}
+          options={{
+            tabBarLabel: 'Gastos',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="view-list" color={color} size={size} />
+            ),
+          }}/>  
+        <Tab.Screen 
+          name="Agregar gasto" 
+          component={AddExpenseScreen} 
+          options={{
+            tabBarLabel: 'Agregar',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="plus-circle-outline" color={color} size={size} />
+            ),
+          }}/>
+        
       </Tab.Navigator>
     </NavigationContainer>
   )
