2027e3480112f36f1cb95fa7e9afe827214133b7
Gerardo Rodriguez <grdwebdev@gmail.com>
2023-10-08 20:17:58 -0600

    Add delete handler. Update styles.

--- src/screens/ListExpensesScreen.js
+++ src/screens/ListExpensesScreen.js
@@ -1,18 +1,31 @@
 import React, {useCallback, useState, useEffect} from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { createTable, getDBConnection, getExpenses } from '../services/db-service';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { createTable, deleteExpense, getDBConnection, getExpenses } from '../services/db-service';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ExpenseList = (props) => {
   const [expenses, setExpenses] = useState([])
 
-  const loadExpensesCallback = 
-    async () => {
+  const onDelete = async (expense) => {
+    const expensesList = expenses.filter( item => item.id !== expense.id)
+    console.log(expense.id)
+    try {
+      const db = await getDBConnection()
+      const result = await deleteExpense(db, expense.id)
+      console.log(result)
+      setExpenses(expensesList)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const loadExpensesCallback = async () => {
+      // console.warn('effect')
       try {
         const db = await getDBConnection();
-        // console.log(db)
         await createTable(db);
         const expenses = await getExpenses(db);
-        console.log(expenses)
+        setExpenses(expenses)
       } catch (error) {
         console.error(error)
       }
@@ -25,14 +38,17 @@ const ExpenseList = (props) => {
   
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Expense List</Text>
       <FlatList
-        data={props.expenses}
+        data={expenses}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <View style={styles.expenseItem}>
-            <Text style={styles.expenseTitle}>{item.title}</Text>
+            <MaterialCommunityIcons name='food' size={20} style={styles.icon} color={'#13561D'}/>
+            <Text style={styles.expenseTitle}>{item.description}</Text>
             <Text style={styles.expenseAmount}>${item.amount.toFixed(2)}</Text>
+            <TouchableOpacity onPress={() => onDelete(item)}>
+              <Text>Eliminar</Text>
+            </TouchableOpacity>
           </View>
         )}
       />
@@ -42,30 +58,36 @@ const ExpenseList = (props) => {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     flex: 1,
-    padding: 10,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    padding: 20,
   },
   expenseItem: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 10,
-    marginVertical: 5,
-    backgroundColor: '#f0f0f0',
+    padding: 15,
+    marginBottom: 15,
+    paddingVertical: 18,
+    borderWidth: 1,
+    borderColor: '#DEDEDE',
     borderRadius: 5,
   },
   expenseTitle: {
-    fontSize: 18,
+    fontFamily: 'WorkSans-Medium',
+    fontSize: 16,
+    color: '#353535'
   },
   expenseAmount: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontFamily: 'WorkSans-Bold',
+    fontSize: 16,
+    color: '#06320D',
   },
+  icon: {
+    backgroundColor: '#D7F4DB',
+    borderRadius: 100,
+    padding: 8,
+  }
 });
 
 export default ExpenseList;
